8ffd8d3163d7be57418fa897d26ef686d1db3231
Suryanandx <suryanandkurup@gmail.com>
2021-06-04 14:28:49 +0530

    6/4/2021 latest update

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -3,7 +3,7 @@ import Currency from 'react-currency-formatter'
 
 function Order({id, amount, amountShipping,items,timestamp,images}) {
     return (
-        <div className="relative border rounded-md">
+        <div key={id} className="relative border rounded-md">
             <div className="flex items-center space-x-10 p-5 bg-gray-100 text-gray-600 text-sm">
                 <div >
                     <p className='font-bold text-xs'>
@@ -26,7 +26,7 @@ function Order({id, amount, amountShipping,items,timestamp,images}) {
                 <div className='flex space-x-6 overflow-x-auto'>
                     {
                         images.map(image => (
-                            <img src={image} className='h-20 object-contain sm:h-32' alt="" />
+                            <img key={image} src={image} className='h-20 object-contain sm:h-32' alt="" />
                         ))
                     }
                 </div>
