1355f1392ae8a0cc00a2092299c73b6d748471f1
Suryanandx <suryanandkurup@gmail.com>
2021-06-04 14:50:21 +0530

    Pushed again latest

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ export default function Home({products}) {
         <title>Ecommerce </title>
       </Head>
      
-<Header/> 
+    <Header/> 
     <main className='max-w-screen-2xl mx-auto'>
       {/* Banner */}
     <Banner/>
@@ -22,12 +22,12 @@ export default function Home({products}) {
 }
 
 export async function getServerSideProps(context){
-  const session = getSession(context)
+  
   const products = await  fetch('https://fakestoreapi.com/products' ).then((res) => res.json())
   return {
     props: {
       products,
-      session
+      
     }
   }
 }
